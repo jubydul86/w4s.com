3f5f813fed7b2a11e4b2673e933101a10a71cf20
Mohammed Jubydul Haque <jubydul.rony@gmail.com>
2014-03-01 21:31:50 +0600

    initial commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+	<title>Work for Software~ Home</title>
+</head>
+<body>
+	<img src="images/banner.png" width="172" />
+    <hr />
+	<h1>Welcome to W4S</h1>
+    <p>
+    	We are providing electronic business solutions for all types of business. We have a skilled team.
+        <p>
+        	<h3>W4S News</h3>
+            <ul>
+            	<li>AGM held on 11.12.2013</li>
+                <li>Launch new project</li>
+                <li>Going to have a tour at 01.01.2014</li>
+            </ul>
+        </p>
+    </p>
+    <br/><br/>
+    <hr/>
+    copyright &copy; All rights reserved by W4S.
+</body>
+</html>
